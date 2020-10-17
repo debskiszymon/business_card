924c09fb0a23ab7db40cb4a124b5c98aeff777a7
debskiszymon <debski.szymon@gmail.com>
2020-10-17 11:58:56 +0200

    business_card.py

--- business_card.py
+++ business_card.py
@@ -8,14 +8,21 @@ class nameCard:
         self.company = company
         self.position = position
         self.email = email
-
+       
     def __str__(self):
-        return f"{self.first_name}, {self.last_name}, {self.company}, {self.position}, {self.email}"
+        return f'{self.first_name} {self.last_name} {self.email}'
 
     def __repr__(self):
         return f"nameCard(first_name={self.first_name}, last_name={self.last_name}, company={self.company}, position={self.position}, email={self.email})"
 
-#name1 = nameCard("Szymon", "Debski", "Mercedes", "Kontroler", "debski")
+    def contact(self):
+        return print(f"Kontaktuję się z {self.first_name}, {self.last_name}, {self.position}, {self.email}")
+
+    @property
+    def name_lenght(self):
+        return len(self.first_name + " " + self.last_name)
+
+name1 = nameCard("Szymon", "Debski", "Mercedes", "Kontroler", "debski")
 #print(name1)
 
 def fake_names():
@@ -24,5 +31,23 @@ def fake_names():
         list.append(nameCard(first_name=fake.first_name(), last_name=fake.last_name(), company=fake.company(), position=fake.job(), email=fake.ascii_company_email()))
     return list
 
+print(name1.name_lenght)
+
+'''
+name_1 = nameCard("Szymon", "Debski", "Mercedes", "Kontroler", "debski@")
+name_2 = nameCard("Gosia", "Misiura", "Loreal", "Dyrektor", "gosia@")
+name_3 = nameCard("Mateusz", "Debski", "Lego", "kulfon", "mati@")
+list2 = [name_1, name_2, name_3]
+
+by_first_name = sorted(list2, key=lambda name_card: name_card.first_name) 
+by_last_name = sorted(list2, key=lambda name_card: name_card.last_name)
+by_email = sorted(list2, key=lambda name_card: name_card.email)
+
+for name in by_email:
+    print("{}, {}, {},".format(name.first_name, name.last_name, name.email))
+'''
+
+'''
 for name in fake_names():
     print("{}, {}, {}, {}, {}".format(name.first_name, name.last_name, name.company, name.position, name.email))
+'''
