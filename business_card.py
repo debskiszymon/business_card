66fbd70267ecffd599b867b6447c4b8188262cb5
debskiszymon <debski.szymon@gmail.com>
2020-10-19 11:09:42 +0200

    business_card.py

--- business_card.py
+++ business_card.py
@@ -1,53 +1,71 @@
 from faker import Faker
 fake = Faker()
 
-class nameCard:
-    def __init__(self, first_name, last_name, company, position, email):
+#Personal card class
+class BaseContact:
+    def __init__(self, first_name, last_name, email, phone_number):
         self.first_name = first_name
         self.last_name = last_name
-        self.company = company
-        self.position = position
         self.email = email
+        self.phone_number = phone_number
        
     def __str__(self):
-        return f'{self.first_name} {self.last_name} {self.email}'
+        return f'{self.first_name} {self.last_name} {self.email} {self.phone_number}'
 
     def __repr__(self):
-        return f"nameCard(first_name={self.first_name}, last_name={self.last_name}, company={self.company}, position={self.position}, email={self.email})"
+        return f"BaseContact(first_name={self.first_name}, last_name={self.last_name}, email={self.email} phone_number={self.phone_number})"
 
     def contact(self):
-        return print(f"Kontaktuję się z {self.first_name}, {self.last_name}, {self.position}, {self.email}")
+        return print(f"Wybieram {self.phone_number} i dzwonię do {self.first_name} {self.last_name}")
 
     @property
     def name_lenght(self):
         return len(self.first_name + " " + self.last_name)
 
-name1 = nameCard("Szymon", "Debski", "Mercedes", "Kontroler", "debski")
+#Business card inheritance class
+class BusinessContact(BaseContact):
+    def __init__(self, position, company, business_phone, business_email, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.position = position
+        self.company = company
+        self.business_phone = business_phone
+        self.business_email = business_email
+
+    def __repr__(self):
+        return f"BusinessContact(first_name={self.first_name}, last_name={self.last_name}, email={self.email} phone_number={self.phone_number}, position={self.position}, company={self.company}, business_phone={self.business_phone}, business_email={self.business_email})"
+
+#name1 = BaseContact("Szymon", "Debski", "debski", 600)
+#name2 = BusinessContact(first_name="Gosia", last_name="Misiura", email="gosia@email.pl", phone_number="500500500", position="Dyrektor", company="Loreal", business_phone="600600600", business_email="gosia.biz@email.pl")
 #print(name1)
 
-def fake_names():
-    list = []
-    for i in range(10):
-        list.append(nameCard(first_name=fake.first_name(), last_name=fake.last_name(), company=fake.company(), position=fake.job(), email=fake.ascii_company_email()))
-    return list
+#fake card creator function
+def create_contacts():
+    type = input("Wpisz rodzaj wizytówki (BaseContact or BusinessContact): ")
+    if type == "BaseContact" or type == "BusinessContact":
+        try:
+            quantity = int(input("Wpisz ile chcesz wygenerować wizytówek: "))
+        except ValueError:
+            print("Proszę podać liczbę!!")
+            exit(1)
+        list_base_contact = []
+        list_busniess_contact = []
+        if type == "BaseContact":
+            for i in range(quantity):
+                list_base_contact.append(BaseContact(first_name=fake.first_name(), last_name=fake.last_name(), email=fake.ascii_email(), phone_number=fake.msisdn()))
+            for name in list_base_contact:
+                print("{}, {}, {}, {}".format(name.first_name, name.last_name, name.email, name.phone_number))
+            return list_base_contact
+        elif type == "BusinessContact":
+            for i in range(quantity):
+                list_busniess_contact.append(BusinessContact(first_name=fake.first_name(), last_name=fake.last_name(), email=fake.ascii_email(),
+                phone_number=fake.msisdn(), position=fake.job(), company=fake.company(), business_phone=fake.msisdn(), business_email=fake.ascii_company_email()))
+            for name in list_busniess_contact:
+                print("{}, {}, {}, {}, {}, {}, {}, {}".format(name.first_name, name.last_name, name.email, name.phone_number, name.position, name.company, name.business_phone, name.business_email))
+            return list_busniess_contact
+    else:
+        exit(1)
+   
+for z in create_contacts():
+    print(z.name_lenght)
 
-print(name1.name_lenght)
 
-'''
-name_1 = nameCard("Szymon", "Debski", "Mercedes", "Kontroler", "debski@")
-name_2 = nameCard("Gosia", "Misiura", "Loreal", "Dyrektor", "gosia@")
-name_3 = nameCard("Mateusz", "Debski", "Lego", "kulfon", "mati@")
-list2 = [name_1, name_2, name_3]
-
-by_first_name = sorted(list2, key=lambda name_card: name_card.first_name) 
-by_last_name = sorted(list2, key=lambda name_card: name_card.last_name)
-by_email = sorted(list2, key=lambda name_card: name_card.email)
-
-for name in by_email:
-    print("{}, {}, {},".format(name.first_name, name.last_name, name.email))
-'''
-
-'''
-for name in fake_names():
-    print("{}, {}, {}, {}, {}".format(name.first_name, name.last_name, name.company, name.position, name.email))
-'''
